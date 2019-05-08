60f202b7a83f39e3ebc6e134ff26970fdb2d43cf
Ruben Meseguer <rubenmeseguer7@gmail.com>
2019-05-08 21:04:31 +0200

    Starting base

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+const contracts = [
+  artifacts.require("./usingOraclize"),
+  artifacts.require("./DieselPrice.sol")
+];
+
+module.exports = deployer =>
+  contracts.map(contract => deployer.deploy(contract));
